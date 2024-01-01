feeacca470a0cfe76b08db902c831e5f6d60f9a2
hiro <h.kaneko.baseball@icloud.com>
2024-01-01 21:16:43 +0900

    Create StorePoint.

---
+++ CocoShibaTsuka/Model/StorePoint.swift
@@ -0,0 +1,8 @@
+//
+//  StorePoint.swift
+//  CocoShibaTsuka
+//
+//  Created by 金子広樹 on 2024/01/01.
+//
+
+import Foundation
---
+++ CocoShibaTsuka/Views/HomeViews/StoreViews/StoreQRCodeView.swift
@@ -0,0 +1,18 @@
+//
+//  StoreQRCodeView.swift
+//  CocoShibaTsuka
+//
+//  Created by 金子広樹 on 2023/12/31.
+//
+
+import SwiftUI
+
+struct StoreQRCodeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoreQRCodeView()
+}
